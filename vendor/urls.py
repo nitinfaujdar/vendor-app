ec3f110b25d8f562398eb936fe90c09bd25ddec5
nitinfaujdar <nitin@squadcam.app>
2023-12-09 17:54:24 +0530

    Added the APIs required for the vendor App

--- vendor/urls.py
+++ vendor/urls.py
@@ -2,5 +2,12 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('vendors/', VendorListView.as_view()),
+    path('vendors/', VendorListView.as_view(), name='vendor-list'),
+    path('vendors/<int:vendor_id>/', VendorListView.as_view(), name='vendor-detail'),
+    path('purchase_orders/', PurchaseOrderListView.as_view(), name='purchase-order-list'),
+    path('purchase_orders/<int:po_id>/', PurchaseOrderListView.as_view(), name='purchase-order-detail'),
+    path('vendors/<int:vendor_id>/performance/', VendorPerformanceView.as_view(), name='vendor-performance'),
+    path('purchase_orders/<int:po_id>/acknowledge/', AcknowledgePurchaseOrderView.as_view(), name='acknowledge-purchase-order'),
+    path('vendors/<int:vendor_id>/historical_performance/', vendor_performance, name='vendor-historical-performance'),
+
 ]
--- vendor/views.py
+++ vendor/views.py
@@ -5,6 +5,9 @@ from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework import status
+from django.shortcuts import get_object_or_404
+from rest_framework import status
+from rest_framework.decorators import api_view
 
 from .models import *
 from .serializers import *
@@ -121,6 +124,37 @@ class PurchaseOrderListView(GenericAPIView):
         obj.delete()
         return Response({"message": "PO deleted successfully"}, status=status.HTTP_200_OK)
 
+class VendorPerformanceView(GenericAPIView):
+    serializer_class = VendorSerializer
+
+    def get(self, request, vendor_id):
+        vendor = get_object_or_404(Vendor, id=vendor_id)
+
+        performance_data = {
+            "on_time_delivery_rate": vendor.on_time_delivery_rate,
+            "quality_rating_avg": vendor.quality_rating_avg,
+            "average_response_time": vendor.average_response_time,
+            "fulfillment_rate": vendor.fulfillment_rate,
+        }
+
+        return Response({"message": "Vendor performance metrics retrieved successfully", "data": performance_data}, 
+                        status=status.HTTP_200_OK)
+
+class AcknowledgePurchaseOrderView(GenericAPIView):
+    serializer_class = PurchaseOrderSerializer
+
+    def post(self, request, po_id):
+        purchase_order = get_object_or_404(PurchaseOrder, id=po_id)
+
+        # Your logic to update acknowledgment_date and trigger recalculation
+        # of average_response_time goes here.
+
+        serializer = self.get_serializer(purchase_order, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response({"message": "Purchase Order acknowledged successfully", "data": serializer.data}, 
+                        status=status.HTTP_200_OK)
 
 @api_view(['GET'])
 def vendor_performance(request, vendor_id):
